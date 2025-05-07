9b9ad326dbb09cd1cf8b94db3500def2a770824a
Gustavo Avide <g_a93@outlook.com>
2025-05-07 19:17:39 +0100

    feat: refactor nav links and enhance styles

---
+++ components/MainNavLink.tsx
@@ -0,0 +1,44 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const MainNavLinks = () => {
+  const links = [
+    {
+      href: "/",
+      label: "Dashboard",
+    },
+    {
+      href: "/tickets",
+      label: "Tickets",
+    },
+    {
+      href: "/users",
+      label: "Users",
+    },
+  ];
+
+  const currentPath = usePathname();
+
+  return (
+    <div className="flex items-center gap-2">
+      {links.map((link) => {
+        return (
+          <Link
+            key={link.label}
+            href={link.href}
+            className={`navbar-link ${
+              currentPath === link.href &&
+              "cursor default text-primary/70 hover:text-primary/60"
+            }`}
+          >
+            {link.label}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MainNavLinks;
