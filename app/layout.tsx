bd1fbc124e50ea0b35328bc1415d3591a7b5415b
Gustavo Avide <g_a93@outlook.com>
2025-05-07 18:45:59 +0100

    feat: prettier files and change update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <MainNav />
-        {children}</body>
+        <nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
+          <div className="max-w-6xl w-full">
+            <MainNav />
+          </div>
+        </nav>
+        <main className="flex flex-col items-center border-b">
+          <div className="max-w-6xl w-full">{children}</div>
+        </main>
+      </body>
     </html>
   );
 }
