2598e8faf3f2ef4410ac43776b45c4f99e61df72
Gustavo Avide <g_a93@outlook.com>
2025-05-09 17:29:33 +0100

    feat: add alert dialog and card from shadcn, add react markdown and typography for talwind css markdown

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,6 +63,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-};
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+} satisfies Config;
+
 export default config;
